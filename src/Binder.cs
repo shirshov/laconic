964f09352aa4f7167d6b83760215227f31408480
Alex Shirshov <shirshov@gmail.com>
2020-07-12 19:45:59 +1000

    Clean up

--- src/Binder.cs
+++ src/Binder.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using xf = Xamarin.Forms;
 using System.Runtime.CompilerServices;
 
--- src/Controls.cs
+++ src/Controls.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq.Expressions;
 using xf = Xamarin.Forms;
 
 namespace Laconic
